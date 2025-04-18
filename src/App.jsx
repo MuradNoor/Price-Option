7569696d09dcbd875e630cc044572bd3504070af
MuradNoor <murad151cse48@gmail.com>
2025-04-18 16:11:47 +0600

    Navbar Started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navbar from './assets/Components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+      <h1 className='text-6xl bg-red-500 p-5 text-center'>Gym Price Option</h1>
+    </>
+  )
+}
+
+export default App
